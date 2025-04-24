6cfdcdcb7b4bad66b18097b033c019da60e512ac
Zhaidar <suholee75@gmail.com>
2025-04-24 11:21:49 +0700

    login/register

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ use App\Http\Controllers\HomeController;
 
 
 Route::get ("/",[HomeController::class,"index"]);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
